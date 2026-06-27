735912868458f054acc01664893261492b625f6c
sp2816 <selin.parmar2006@gmail.com>
2026-06-27 15:42:05 +0530

    Fix component filename casing

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const AUTH_API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export default AUTH_API;
